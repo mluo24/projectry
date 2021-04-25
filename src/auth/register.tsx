b90b4a5f84f45af0c4cac2f55a684017ad2d8d07
Taerim Eom <taeirm.eom.official@gmail.com>
2021-04-25 18:03:03 -0400

    made a separate folder for auth

--- src/auth/register.tsx
+++ src/auth/register.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import admin from 'firebase-admin';
-import firebase from 'firebase/app';
-import express from 'express';
+//import express from 'express';
 
 const serviceAccount = require('../service-account.json');
 
 // initialize firebase app
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount)
 });
 
 
 const Authentication = () => {
 
-    return (
-        <div>
-        </div>
-    );
+  return (
+    <div>
+    </div>
+  );
 
 }
 
